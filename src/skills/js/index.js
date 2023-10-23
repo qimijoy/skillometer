b04c42c63ffb70a4177c839bcae9b41afc2f7157
qimijoy <qimijoy@yandex.ru>
2023-10-23 13:10:24 +0300

    rest event loop tasks

--- src/skills/js/index.js
+++ src/skills/js/index.js
@@ -4,10 +4,14 @@ import task3 from './0003.json';
 import task4 from './0004.json';
 import task5 from './0005.json';
 import task6 from './0006.json';
+import task7 from './0007.json';
+import task8 from './0008.json';
+import task9 from './0009.json';
+import task10 from './0010.json';
 
 const base = process.env.NODE_ENV === 'production' ? '/skillometer/' : '/';
 
-const tasks = [task1, task2, task3, task4, task5, task6];
+const tasks = [task1, task2, task3, task4, task5, task6, task7, task8, task9, task10];
 tasks.forEach((task) => {
 	task.images = task.images.map((image) => base + image);
 });
--- src/skills/vue/index.js
+++ src/skills/vue/index.js
@@ -1,3 +1,3 @@
-// import task1 from './1.json';
+// import task1001 from './1001.json';
 
 export default {};
