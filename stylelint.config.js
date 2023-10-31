d8e787a9607ffba214a8f9b207ba2fddc8f5d328
qimijoy <qimijoy@yandex.ru>
2023-10-31 12:24:33 +0300

    stylelint config update

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-	ignoreFiles: ['**/dist/**'],
+	ignoreFiles: ['**/node_modules/**', '**/dist/**'],
+
 	extends: ['stylelint-config-standard'],
 	plugins: ['stylelint-order'],
+
 	defaultSeverity: 'error',
+
+	reportDescriptionlessDisables: true,
+	reportInvalidScopeDisables: true,
+	reportNeedlessDisables: true,
+	configurationComment: 'stylelint', // --> /* stylelint-disable */
+	ignoreDisables: true,
+
 	rules: {
 		'order/order': ['custom-properties', 'declarations'],
 		'order/properties-order': ['width', 'height'],
